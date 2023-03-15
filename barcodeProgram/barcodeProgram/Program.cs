b6ac8a05b728999b2431ef5db33e1108e6734d33
lj416 <126787328+lj416@users.noreply.github.com>
2023-03-15 10:48:05 +0000

    Uploaded program files
    Visual Studio files

---
+++ barcodeProgram/barcodeProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+bool checkInput = false;
+while (checkInput != true)
+{
+    try
+    {
+        Console.WriteLine("Enter barcode: ");
+        string barcodeInput = Console.ReadLine();
+        int intBarcodeInput = Int32.Parse(intBarcodeInput);
+    }
+    catch
+    {
+        Console.WriteLine("Bad Input");
+    }
+
+}
